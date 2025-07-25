a7d119779b5b4c0a1926d808272fb75506510fa7
rafael.choinhet <choinhet@gmail.com>
2025-07-25 11:13:54 -0300

    chore: form updates

--- testing_htmx/main.py
+++ testing_htmx/main.py
@@ -1,8 +1,9 @@
 import logging
+from typing import List, Annotated
 
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request, Depends, Form
 from jinja2_fragments.fastapi import Jinja2Blocks
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -11,8 +12,27 @@ logger = logging.getLogger("uvicorn")
 logger.setLevel(logging.INFO)
 
 
+class Contact(BaseModel):
+    name: str
+    email: str
+
+
+def get_some_contacts() -> List[Contact]:
+    return [
+        Contact(name="John", email="jd@gmail.com"),
+        Contact(name="Clara", email="cd@gmail.com"),
+    ]
+
+
+def has_email(contacts: List[Contact], email: str) -> bool:
+    for contact in contacts:
+        if email == contact.email:
+            return True
+    return False
+
+
 class Message(BaseModel):
-    count: int = 0
+    contacts: List[Contact] = Field(default_factory=get_some_contacts)
 
 
 message = Message()
@@ -30,19 +50,30 @@ async def root(
     return templates.TemplateResponse(
         request,
         "index.html.j2",
-        dict(count=message.count)
+        message.model_dump()
     )
 
 
-@app.post("/count")
-async def root(
+@app.post("/contacts")
+async def contacts(
         request: Request,
+        name: Annotated[str, Form()],
+        email: Annotated[str, Form()],
         message: Message = Depends(get_message)
 ):
-    message.count += 1
+    if has_email(message.contacts, email):
+        return templates.TemplateResponse(
+            request,
+            "index.html.j2",
+            message.model_dump(),
+            block_name="display",
+            status_code=422,
+        )
+
+    message.contacts.append(Contact(name=name, email=email))
     return templates.TemplateResponse(
         request,
         "index.html.j2",
-        dict(count=message.count),
-        block_name="count"
+        message.model_dump(),
+        block_name="display",
     )
